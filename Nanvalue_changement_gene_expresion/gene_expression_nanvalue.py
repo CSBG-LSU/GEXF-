c7bc993d26310cb9d382c453a26a17092be5754e
Guannan1900 <54994988+Guannan1900@users.noreply.github.com>
2019-10-02 16:40:58 -0500

    Add files via upload

---
+++ Nanvalue_changement_gene_expresion/gene_expression_nanvalue.py
@@ -0,0 +1,48 @@
+"""
+Version: 1.0
+Author: Guannan
+"""
+# -*- coding: utf-8 -*-
+import pandas as pd
+import networkx as nx
+import numpy as np
+import argparse
+import time
+
+
+def getArgs():
+    parser = argparse.ArgumentParser('python')
+    parser.add_argument('-inputfile', required=True)
+    parser.add_argument('-outputfile', required=True)
+    return parser.parse_args()
+
+
+def create_network(inputfile, outputfile):
+    Graph = nx.read_gexf(inputfile)
+    nodesNAN = [x for x, y in Graph.nodes(data=True) if y['Gene-exp'] == 'nan']  # find nodes Gene-exp=nan
+    num = len(nodesNAN)
+    for i in range(num):
+        nan_neighbors = [n for n in Graph[nodesNAN[i]]]
+        num_nan_neighbors = len(nan_neighbors)
+        temp2 = ['0.0'] * num_nan_neighbors
+        for j in range(num_nan_neighbors):
+            temp1 = Graph.nodes[nan_neighbors[j]]['Gene-exp']
+            temp2[j] = float(temp1)
+        sum1 = np.sum(temp2)
+        if sum1 > 0.0:
+            sum1 = 1.0
+        elif sum1 < 0.0:
+            sum1 = -1.0
+        else:
+            sum1 = 0.0
+        Graph.nodes[nodesNAN[i]]['Gene-exp'] = sum1
+    nx.write_gexf(Graph, outputfile)
+
+
+if __name__ == "__main__":
+    args = getArgs()
+    create_network(args.inputfile, args.outputfile)
+    start = time.time()
+    end = time.time()
+
+    print('time elapsed:' + str(end - start))
